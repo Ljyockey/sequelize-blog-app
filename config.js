73e489d9742901ffabd3d6ead4079d0034bd2de3
ljyockey <linyockey@gmail.com>
2017-05-15 18:22:08 -0700

    added configurations

---
+++ config.js
@@ -0,0 +1,19 @@
+const env = process.env.NODE_ENV || 'development'
+
+const DATABASE_URL = (
+	process.env.DATABASE_URL ||
+	global.DATABASE_URL ||
+	'postgres://localhost/dev-blog-app'
+	);
+
+	const TEST_DATABASE_URL = (
+		process.env.TEST_DATABASE_URL ||
+		global.TEST_DATABASE_URL ||
+		'postgres://localhost/test-blog-app'
+		);
+
+	module.exports = {
+		PORT: process.env.PORT || 8080,
+		DATABASE_URL: env === 'test' ? TEST_DATABASE_URL : DATABASE_URL,
+		SEQUELIZE_OPTIONS: {logging: env === 'test' ? false : console.log}
+	};
