18e1751f5c92f9e3982af6b2891fe1544726a1ba
ljyockey <linyockey@gmail.com>
2017-05-16 21:57:25 -0700

    added GET requests

--- routes/authors.js
+++ routes/authors.js
@@ -3,5 +3,54 @@ const router = express.Router();
 
 const {Author, Comment, Post} = require('../models');
 
+//get author by ID
+router.get('/:id', (req, res) => Author.findById(req.params.id, {
+	include: [{
+		model: Post,
+		as: 'posts'
+	},
+	{
+		model: Comment,
+		as: 'comments'
+	}]
+})
+.then(author => res.json({
+	authors: author.apiRepr()
+}))
+);
+
+//get author's posts by author ID
+router.get('/:id/posts', (req, res) => {
+	return Author.findById(req.params.id, {
+		include: [{
+			model: Post,
+			as: 'posts'
+		},
+		{
+			model: Comment,
+			as: 'comments'
+		}]
+	})
+	.then(author => 
+		res.json({posts: author.posts.map(post => post.apiRepr())
+		}));
+});
+
+//gets all comments from author
+router.get('/:id/comments', (req, res) => {
+	Author.findById(req.params.id, {
+		include: [{
+			model: Post,
+			as: 'posts'
+		},
+		{
+			model: Comment,
+			as: 'comments'
+		}]
+	})
+	.then(author =>
+		res.json({comments: author.comments.map(comment => comment.apiRepr())
+		}));
+});
 
 module.exports = router;
--- routes/posts.js
+++ routes/posts.js
@@ -4,4 +4,31 @@ const router = express.Router();
 const {Author, Post, Comment} = require('../models');
 
 
+//get all posts
+router.get('/', (req, res) =>
+	Post.findAll({
+		include: {
+			model: Comment,
+			as: 'comments'
+		}
+	})
+	.then(posts => 
+		res.json({posts: posts.map(post => post.apiRepr())
+		})));
+
+//get all comments for post
+router.get('/:id/comments', (req, res) => {
+	return Post.findById(req.params.id, {
+		include: {
+			model: Comment,
+			as: 'comments'
+		}
+	})
+	.then(post => {
+		res.json({comments: post.comments.map(comment => 
+			comment.apiRepr())
+	});
+	});
+});
+
 module.exports = router;
