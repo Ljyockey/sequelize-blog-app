e0227e3a5b94ec9a5c9f38f78eb1d965d5c66880
ljyockey <linyockey@gmail.com>
2017-05-16 19:45:59 -0700

    fixed minor syntax errors

--- migrations/migrations/001-blog-posts-initial.sql
+++ migrations/migrations/001-blog-posts-initial.sql
@@ -2,8 +2,8 @@ BEGIN;
 
 CREATE TABLE authors (
 	id SERIAL PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL;
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
 	first_name TEXT,
 	last_name TEXT,
 	username TEXT NOT NULL
@@ -11,17 +11,17 @@ CREATE TABLE authors (
 
 CREATE TABLE posts (
 	id SERIAL PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
 	title TEXT NOT NULL,
 	content TEXT,
-	author_id REFERENCES authors NOT NULL
+	author_id INTEGER REFERENCES authors NOT NULL
 );
 
 CREATE TABLE comments (
 	id SERIAL PRIMARY KEY,
-	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
 	comment_text TEXT NOT NULL,
 	author_id INTEGER REFERENCES authors ON DELETE CASCADE NOT NULL,
 	post_id INTEGER REFERENCES posts ON DELETE CASCADE NOT NULL
