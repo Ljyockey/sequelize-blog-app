3d540dd7335098bc996d404f4578506b6a2bca4f
ljyockey <linyockey@gmail.com>
2017-05-17 20:03:11 -0700

    added runServer and closeServer functions

---
+++ test/helper.js
@@ -0,0 +1,12 @@
+const {PORT} = require('../config');
+const {runServer, closeServer} = require('../server');
+const {sequelize} = require('../db/sequelize');
+
+before(function() {
+	return sequelize.sync({force: true})
+	.then(() => runServer(PORT));
+});
+
+after(function() {
+	return closeServer();
+});
