1ba4ac7ad7985217805bb15f1bfa186b4f0e6a45
ljyockey <linyockey@gmail.com>
2017-05-17 20:46:03 -0700

    added seed data and beforeEach funcitons

---
+++ test/test-posts-endpoints.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const faker = require('faker');
+const should = chai.should();
+const app = require('../app');
+const {Comment, Author, Post} = require('../models');
+
+chai.use(chaiHttp);
+
+describe('Posts API resource', function() {
+
+	beforeEach(function() {
+		return dropTable()
+		.then(function() { 
+			return seedTestData();
+		});
+	});
+
+
+});
+
