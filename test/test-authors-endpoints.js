c0b21c528754597987defb5d98663295b181f877
ljyockey <linyockey@gmail.com>
2017-05-17 21:17:04 -0700

    added GET tests

--- test/test-authors-endpoints.js
+++ test/test-authors-endpoints.js
@@ -4,6 +4,7 @@ const faker = require('faker');
 const should = chai.should();
 const app = require('../app');
 const {Comment, Author, Post} = require('../models');
+const {buildAuthor, buildPost, buildComment, dropTables, seedTestData} = require('./helper');
 
 chai.use(chaiHttp);
 
@@ -11,11 +12,41 @@ chai.use(chaiHttp);
 describe('Authors API resource', function() {
 
 	beforeEach(function() {
-		return dropTable()
+		return dropTables()
 		.then(function() { 
 			return seedTestData();
 		});
 	});
 
+	describe('GET endpoints', function() {
+
+		it('should return author by ID', function() {
+			let author;
+			return Author.findOne()
+			.then(function(_author) {
+				author = _author;
+				return chai.request(app)
+				.get(`/authors/${author.id}`)
+			})
+			.then(function(res) {
+				res.should.have.status(200);
+				res.body.id.should.equal(author.id);
+			});
+		});
+
+		it('should return posts by author ID', function() {
+			let author;
+			return Author.findOne()
+			.then(function(_author) {
+				author = _author;
+				return chai.request(app)
+				.get(`/authors/${author.id}/posts`)
+			})
+			.then(function(res) {
+				res.should.have.status(200);
+			});
+		});
+
+	});
 
 });
